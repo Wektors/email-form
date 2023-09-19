78158f407f95806af73f4ccc2cdb5df5e9d2bb8d
Wektors <wiktorm16@gmail.com>
2023-09-19 21:29:41 +0200

    form complete without validation

--- src/components/FormLayout.vue
+++ src/components/FormLayout.vue
@@ -1,21 +1,79 @@
 <template>
-  <div class="first-step" v-show='firstStepShow' >
-    <FormEntry label="Imię" v-model="firstName" />
-    <FormEntry label="Drugie imię" v-model="secondName" />
-    <FormEntry label="Nazwisko" v-model="surName" />
-    
-    <br />
-    imie: {{ firstName }}
-    <br />
-    
-    drugie imie: {{ secondName }}
-    <br />
-    
-    nazwisko: {{ surName }}
+	<div class="first-step" v-show="firstStepShow">
+		<FormEntry label="Imię" v-model="firstName" />
+		<FormEntry label="Drugie imię" v-model="secondName" />
+		<FormEntry label="Nazwisko" v-model="surName" />
+
   </div>
+  <br />
 
-  <button @click="triggerNextStep">Przejdź dalej</button>
+	<button @click="triggerNextStep(firstStepShow)" v-show="firstStepShow">
+		Przejdź dalej
+	</button>
 
+
+	<div class="second-step" v-show="secondStepShow">
+		<FormEntry label="email" v-model="email" type="email" />
+		<FormEntry label="numer telefonu" v-model="phone" type="phone" />
+		<FormEntry label="ulica" v-model="street" />
+		<FormEntry label="numer domu" v-model="houseNumber" />
+		<FormEntry label="numer lokalu" v-model="apartmentNumber" />
+		<FormEntry label="kod pocztowy" v-model="postalCode" />
+		<FormEntry label="miasto" v-model="city" />
+
+		<br />
+
+    <br />
+    <button @click="triggerNextStep(firstStepShow)" v-show="secondStepShow">
+		Cofnij
+	</button>
+		<button @click="triggerSummary()" v-show="secondStepShow">
+			Przejdź do podsumowania
+		</button>
+	</div>
+
+
+	<div class="summary" v-show="summaryShow">
+
+    <h2>Podsumowanie: </h2>
+		<br />
+		imie: {{ firstName }}
+		<br />
+
+		drugie imie: {{ secondName }}
+		<br />
+
+		nazwisko: {{ surName }}
+
+    <br />
+
+		email: {{ email }}
+
+    <br />
+
+		telefon: {{ phone }}
+
+    <br />
+
+		ulica: {{ street }}
+
+    <br />
+
+		numer domu: {{ houseNumber }}
+
+    <br />
+
+		numer mieszkania: {{ apartmentNumber }}
+
+    <br />
+
+		kod pocztowy: {{ postalCode }}
+
+    <br />
+
+		miasto: {{ city }}
+
+	</div>
 </template>
 
 <script>
@@ -29,20 +87,34 @@ export default {
 	},
 	data() {
 		return {
-      firstStepShow: true,
+			firstStepShow: true,
+			secondStepShow: false,
+			summaryShow: false,
 			firstName: "",
 			secondName: "",
-			surName: "",
+			email: "",
+			phone: "",
+			street: "",
+			houseNumber: "",
+			apartmentNumber: "",
+			postalCode: "",
+			city: "",
 		};
 	},
-  methods: {
-    triggerNextStep() {
-      this.firstStepShow = !this.firstStepShow
-    }
-  }
+	methods: {
+		triggerNextStep() {
+			this.firstStepShow = !this.firstStepShow;
+			this.secondStepShow = !this.secondStepShow;
+		},
+		triggerSummary() {
+			this.firstStepShow = false;
+			this.secondStepShow = false;
+			this.summaryShow = !this.summaryShow;
+		},
+	},
 };
 </script>
 
 <style scoped>
-  
+   
 </style>
